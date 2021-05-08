e0bbb9039659d4e911d483bc80ffab3f8edd97b2
bywahjoe <wahjoe.why@gmail.com>
2021-05-08 20:41:34 +0700

    V2.0

--- Board ESP/espku/espku.ino
+++ Board ESP/espku/espku.ino
@@ -33,25 +33,43 @@ BLYNK_WRITE(V10) {
 
 }
 void notif() {
-  if (valph < 6.50 || valph > 8.00) Blynk.notify("PH is Not 6.50 - 8");
-  else if (valsuhu < 20 || valsuhu > 30) Blynk.notify("Temp is Not 20 - 30 Celcius");
+  Serial.println("Blynk is Notify");
+  if (valph < 6.50 || valph > 8.00) Blynk.notify(String(valph) + " |PH is Not 6.50 - 8");
+  else if (valsuhu < 20 || valsuhu > 30) Blynk.notify(String(valsuhu) + " |Temp is Not 20 - 30 Celcius");
 }
 void setup() {
+  lcd.init();
+  lcd.backlight();
   pinMode(relay, OUTPUT);
+  pinMode(pin_sensor_temp, INPUT);
+
   digitalWrite(relay, HIGH);
   Serial.begin(115200);
 
   WiFi.begin(ssid, pass);
+  int retry = 1;
   while (WiFi.status() != WL_CONNECTED) {
     WiFi.begin(ssid, pass);
-    Serial.print(".");
-    delay(5000);
+    lcd.setCursor(0, 0);
+    lcd.print("Connecting WiFi Ewake");
+    lcd.setCursor(1, 1);
+    lcd.print(retry);
+    retry++;
+    delay(500);
   }
 
   ThingSpeak.begin(client);
   Blynk.begin(myblynk, ssid, pass);
   sensors.begin ();
   ceknotif.setInterval(7000L, notif);
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("WiFi Connected");
+  lcd.setCursor(4, 1);
+  lcd.print("EWAKE START");
+  delay(2000);
+
 }
 
 void loop() {
@@ -61,31 +79,30 @@ void loop() {
   valph = cekPH();
   valsuhu = cekSuhu();
 
-  #ifdef SHOWDEBUG
-    Serial.print("MYPH   : ");Serial.println(valph);
-    Serial.print("MYSuhu : ");Serial.println(valsuhu);
-  #endif
-  
-  tampil(valph, valsuhu);
-  
-  if (now - before >= interval) {
-    before = now;
-    sendTS();
-  }
+#ifdef SHOWDEBUG
+  Serial.print("MYPH   : "); Serial.println(valph);
+  Serial.print("MYSuhu : "); Serial.println(valsuhu);
+#endif
 
-  delay(20);
+  tampil(valph, valsuhu);
+
+  if (now - before >= interval) {
+    sendTS();
+    before = now;
+  }
+  //delay(20);
   Blynk.run();
   ceknotif.run();
 }
 void sendTS() {
-  Serial.print("PH   : "); Serial.println(valph);
-  Serial.print("SUHU : "); Serial.println(valsuhu);
+  //  Serial.print("PH   : "); Serial.println(valph);
+  //  Serial.print("SUHU : "); Serial.println(valsuhu);
 
   ThingSpeak.setField(1, valph);
   ThingSpeak.setField(2, valsuhu);
 
   int x = ThingSpeak.writeFields(myid, myapi);
-  if (x == 200)    Serial.println("Channel update successful.");
+  if (x == 200)    Serial.println("[TS]Channel update successful.");
   else  Serial.println("Problem updating channel. HTTP error code " + String(x));
 
 }
@@ -98,10 +115,10 @@ void setOFF()  {
 void tampil(float myph, float mysuhu) {
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("Nilai PH   :");
+  lcd.print("PH   :");
   lcd.print(myph);
   lcd.setCursor(0, 1);
-  lcd.print("Nilai Suhu :");
+  lcd.print("Temp :");
   lcd.print(mysuhu);
   lcd.print((char)223);
   lcd.print("C");
@@ -111,7 +128,7 @@ float cekSuhu() {
   sensors.requestTemperatures();
   val = sensors.getTempCByIndex(0);
 
-  Serial.print("Suhu : "); Serial.println(val);
+  //  Serial.print("Suhu : "); Serial.println(val);
   return val;
 }
 float cekPH() {
@@ -122,7 +139,7 @@ float cekPH() {
   nilaiAnalog = analogRead(pin_sensor_ph);
 
   //rumus nilai tegangan
-  voltage = nilaiAnalog * (5.0 / 1023);
+  voltage = nilaiAnalog * (5.0 / 4095);
 
   //rumus nilai ph air
   nilaiPH = 2.676 * voltage - 0.3130;;
@@ -130,12 +147,12 @@ float cekPH() {
   else if (nilaiPH > 6.00) nilaiPH = nilaiPH + 0.02;
   else if (nilaiPH < 4.00) nilaiPH = nilaiPH - 0.01;
 
-  Serial.print("Nilai Analog : ");
-  Serial.print(nilaiAnalog);
-  Serial.print("      Nilai Voltage : ");
-  Serial.print(voltage);
-  Serial.print("          Nilai PH : ");
-  Serial.print(nilaiPH);
+  //  Serial.print("Nilai Analog : ");
+  //  Serial.print(nilaiAnalog);
+  //  Serial.print("      Nilai Voltage : ");
+  //  Serial.print(voltage);
+  //  Serial.print("          Nilai PH : ");
+  //  Serial.print(nilaiPH);
 
   return nilaiPH;
 }
--- Board ESP/espku/pinku.h
+++ Board ESP/espku/pinku.h
@@ -1,7 +1,6 @@
-#define pin_sensor_ph 18
-#define pin_sensor_temp 15
+#define pin_sensor_ph 34
+#define pin_sensor_temp 32
 #define relay 10
-
 
 #define myapi "3Q78NW6JQJD18X4T"
 #define myid  1383116
